af98a3a8c6c5b0c0dfce396d2ca2f5a479cf6607
Alex Ilyaev <alexilyaev@gmail.com>
2015-09-26 19:22:50 +0300

    Updated to single quotes

--- common/module.js
+++ common/module.js
@@ -3,6 +3,6 @@
 angular.module('frapontillo.bootstrap-switch', []);
 
 // Common.js package manager support (e.g. ComponentJS, WebPack)
-if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports) {
+if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.exports === exports) {
   module.exports = 'frapontillo.bootstrap-switch';
 }
