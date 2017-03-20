67b1755122601d4a355211a188c57b737c1eb224
johnz <john.zwarthoed@vancis.nl>
2017-03-20 14:50:14 +0100

    Fix jquery import in tests

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'bower_components/jquery/dist/jquery.js',
+      'bower_components/jquery/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/bootstrap-switch/dist/js/bootstrap-switch.js',
